733955e546ce49283e8e61ac44c917c84882a9ba
João Vitor Ianuci <joaoianuci@gmail.com>
2024-01-16 14:18:41 -0300

    feat: adding cursor pointer

--- app/components/nav/Navbar.tsx
+++ app/components/nav/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export const Navbar = () => {
   return (
     <nav className="flex flex-row justify-between items-center min-w-[100%]">
-      <div className="flex flex-row gap-3 justify-center items-start">
+      <div className="flex flex-row gap-3 justify-center items-start cursor-pointer">
         <Image src="logo.svg" alt="logo" width={40} height={40} />
         <span className="text-white font-normal text-2xl jura">CodeNFT</span>
       </div>
